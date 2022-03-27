ef27bc8c3728d2ba1e579d2b20fb8363cfd7ff69
Michelle Santos <michelle.rigelo@gmail.com>
2022-03-27 13:44:25 -0300

    implementando teste.

--- previsao/profeta.py
+++ previsao/profeta.py
@@ -12,9 +12,12 @@ def prever_casos_futuros(novos_casos_covid_csv, dias):
 
     quadro_de_dados_futuro = modelo.make_future_dataframe(periods=dias)
     previsao = modelo.predict(quadro_de_dados_futuro)
-    mostrar_previsao(previsao, dias)
+    return previsao
 
 
 def mostrar_previsao(previsao, dias):
+    previsao_dias = ''
     for index, yhat in enumerate(previsao.yhat[-dias:]):
-        print(f'{index+1} -> {round(yhat)}')
+        previsao_dias += f'{index+1} -> {round(yhat)}\n'
+
+    return previsao_dias
--- principal.py
+++ principal.py
@@ -1,5 +1,5 @@
 from modelo.novos_casos_csv import NovosCasosCovidCsv
-from previsao.profeta import prever_casos_futuros
+from previsao.profeta import prever_casos_futuros, mostrar_previsao
 
 
 def input_dias():
@@ -12,4 +12,5 @@ def input_dias():
 novos_casos_arquivo_csv = NovosCasosCovidCsv()
 novos_casos_arquivo_csv.preencher_arquivo_csv('new_cases.csv')
 dias = input_dias()
-prever_casos_futuros(novos_casos_arquivo_csv, dias)
+previsao = prever_casos_futuros(novos_casos_arquivo_csv, dias)
+print(mostrar_previsao(previsao, dias))
