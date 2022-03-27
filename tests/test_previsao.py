3ed9c279acd114797d6a2dd86264ab08b52c7469
Michelle Santos <michelle.rigelo@gmail.com>
2022-03-27 14:51:51 -0300

    corrigindo diretorio do testes para CI.

--- tests/test_previsao.py
+++ tests/test_previsao.py
@@ -4,7 +4,7 @@ from previsao.profeta import prever_casos_futuros, mostrar_previsao
 
 def test_deve_gerar_previsao():
     novos_casos_covid_csv = NovosCasosCovidCsv()
-    novos_casos_covid_csv.preencher_arquivo_csv('new_cases_test.csv')
+    novos_casos_covid_csv.preencher_arquivo_csv('tests/new_cases_test.csv')
 
     dias = 4
     previsao = prever_casos_futuros(novos_casos_covid_csv, dias)
